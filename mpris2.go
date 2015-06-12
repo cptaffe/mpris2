9ec78e52aa6e173827b1cf08baf4d53ab5be8813
Connor Taffe <cpaynetaffe@gmail.com>
2015-06-11 23:14:07 -0500

    update

--- mpris2.go
+++ mpris2.go
@@ -3,107 +3,49 @@ package mpris2
 import dbus "github.com/guelfey/go.dbus"
 
 type Player struct {
-	Name string // name of the player, e.g "spotify"
+	Name string // name of the player
+	obj *dbus.Object // associated dbus object
 }
 
-func Players() (players []*Player, err error) {
-	conn, err := dbus.SessionBus()
-	if err != nil {
-		return
-	}
-
-	names := conn.Names()
-	players = make([]*Player, len(names))
-
-	for i, name := range names {
-		players[i] = NewPlayer(name)
-	}
-	return
-}
-
-func NewPlayer(name string) *Player {
+func NewPlayer(name string) (*Player, error) {
 	player := new(Player)
 	player.Name = "org.mpris.MediaPlayer2." + name
-	return player
-}
 
-func (player *Player) object() (obj *dbus.Object, err error) {
 	conn, err := dbus.SessionBus()
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	obj = conn.Object(player.Name, "/org/mpris/MediaPlayer2")
-	return
+	player.obj = conn.Object(player.Name, "/org/mpris/MediaPlayer2")
+
+	return player, err
 }
 
-func (player *Player) Next() (err error) {
-	obj, err := player.object()
-	if err != nil {
-		return
-	}
-
-	obj.Call("org.mpris.MediaPlayer2.Player.Next", 0)
-	return
+func (player *Player) Next() {
+	player.obj.Call("org.mpris.MediaPlayer2.Player.Next", 0)
 }
 
-func (player *Player) Previous() (err error) {
-	obj, err := player.object()
-	if err != nil {
-		return
-	}
-
-	obj.Call("org.mpris.MediaPlayer2.Player.Previous", 0)
-	return
+func (player *Player) Previous() {
+	player.obj.Call("org.mpris.MediaPlayer2.Player.Previous", 0)
 }
 
-func (player *Player) Pause() (err error) {
-	obj, err := player.object()
-	if err != nil {
-		return
-	}
-
-	obj.Call("org.mpris.MediaPlayer2.Player.Pause", 0)
-	return
+func (player *Player) Pause() {
+	player.obj.Call("org.mpris.MediaPlayer2.Player.Pause", 0)
 }
 
-func (player *Player) PlayPause() (err error) {
-	obj, err := player.object()
-	if err != nil {
-		return
-	}
-
-	obj.Call("org.mpris.MediaPlayer2.Player.PlayPause", 0)
-	return
+func (player *Player) PlayPause() {
+	player.obj.Call("org.mpris.MediaPlayer2.Player.PlayPause", 0)
 }
 
-func (player *Player) Stop() (err error) {
-	obj, err := player.object()
-	if err != nil {
-		return
-	}
-
-	obj.Call("org.mpris.MediaPlayer2.Player.Stop", 0)
-	return
+func (player *Player) Stop() {
+	player.obj.Call("org.mpris.MediaPlayer2.Player.Stop", 0)
 }
 
-func (player *Player) Play() (err error) {
-	obj, err := player.object()
-	if err != nil {
-		return
-	}
-
-	obj.Call("org.mpris.MediaPlayer2.Player.Play", 0)
-	return
+func (player *Player) Play() {
+	player.obj.Call("org.mpris.MediaPlayer2.Player.Play", 0)
 }
 
 // TODO: fix this function
-func (player *Player) Seek(offset uint32) (err error) {
-	obj, err := player.object()
-	if err != nil {
-		return
-	}
-
-	obj.Call("org.mpris.MediaPlayer2.Player.Seek", 0, offset)
-	return
+func (player *Player) Seek(offset uint32) {
+	player.obj.Call("org.mpris.MediaPlayer2.Player.Seek", 0, offset)
 }
